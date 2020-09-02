a19f087ffaa30cdd22a8bd0ebb6610e593333ad9
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-09-02 23:03:48 +0300

    add temp data for cells

--- ProductsFromErokhin/Start/StartViewController.swift
+++ ProductsFromErokhin/Start/StartViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class StartViewController: UIViewController {
 
+    @IBOutlet weak var products: UICollectionView!
+    @IBOutlet weak var products2: UICollectionView!
+    private let dispose = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        bind()
     }
-
+    
+    private func bind() {
+        let data = Observable.just(["Product1", "Product2", "Product3", "Product4"])
+        data.bind(to: products.rx.items(cellIdentifier: "product", cellType: ProductCell.self)) { index, model, cell in
+            cell.name.text = model
+        }.disposed(by: dispose)
+        
+        let data2 = Observable.just(["Product5", "Product6", "Product7", "Product8"])
+        data2.bind(to: products2.rx.items(cellIdentifier: "product", cellType: ProductCell.self)) { index, model, cell in
+            cell.name.text = model
+        }.disposed(by: dispose)
+    }
 
 }
 
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+}
