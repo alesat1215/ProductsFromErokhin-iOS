c94e1ab295a0fea82643b2882de5221d8596849f
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-09-02 14:50:26 +0300

    Initial Commit

---
+++ ProductsFromErokhin/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ProductsFromErokhin
+//
+//  Created by Alexander Satunin on 02.09.2020.
+//  Copyright © 2020 Alexander Satunin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProductsFromErokhin/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ProductsFromErokhin
+//
+//  Created by Alexander Satunin on 02.09.2020.
+//  Copyright © 2020 Alexander Satunin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
