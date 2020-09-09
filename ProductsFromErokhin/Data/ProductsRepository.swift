5ed53fe16cd2a8444c3a1a2c695a87ca347cf15a
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-09-09 23:28:47 +0300

    del comments

--- ProductsFromErokhin/Data/ProductsRepository.swift
+++ ProductsFromErokhin/Data/ProductsRepository.swift
@@ -29,67 +29,15 @@ class ProductsRepository {
         self.remoteConfigRepository = remoteConfigRepository
         self.decoder = decoder
     }
-    /** Get data from remote confic & decode it from JSON */
-//    func productsAndGroups() -> Observable<[Group]>? {
-//        remoteConfigRepository?.fetchAndActivate()?.map { [weak self] in
-//            switch $0 {
-//            case .failure(let error):
-//                  throw error
-//            default:
-//                guard let self = self else {
-//                    return []
-//                }
-//                // Check di
-//                guard let decoder = self.decoder,
-//                    let remoteConfig = self.remoteConfigRepository?.remoteConfig
-//                    else { throw AppError.productsRepositoryDI }
-//                // Decode JSON
-//                let products = try decoder.decode(
-//                    [Group].self,
-//                    from: remoteConfig["products"].dataValue)
-//                self.updateDB(groups: products)
-//                return products
-//            }
-//
-//        }
-//    }
+
     
     func groups() -> Observable<Result<[Group], Error>> {
-//        return context?.rx.entities(fetchRequest: Group.fetchRequestWithSort())
         Observable.merge([_groups, remoteConfigRepository!.fetchAndActivate()])
-//        _groups
     }
     
     func products() -> Observable<Result<[Product], Error>> {
-//        let products: Observable<[GroupRemote]>? = remoteConfigRepository?.remoteData(key: "products")
-//        return products?.flatMap { [weak self] result -> Observable<[Product]> in
-//            guard let self = self, let context = self.context else {
-//                return Observable.empty()
-//            }
-//            try self.updateDB(groups: result)
-//            return context.rx.entities(fetchRequest: Product.fetchRequestWithSort())
-//        }
         Observable.merge([_products, remoteConfigRepository!.fetchAndActivate()])
     }
-    
-//    private func updateDB(groups: [GroupRemote]) throws {
-//        
-//        guard let context = context else {
-//            print("Context is nil. Nothing to update")
-//            throw AppError.productsRepositoryDI
-//        }
-//                
-//        try Group.clearEntity(context: context)
-//        
-//        var productOrder = 0
-//        groups.enumerated().forEach {
-//            context.insert($1.managedObject(context: context, groupOrder: $0, productOrder: &productOrder))
-//        }
-//        
-//        if context.hasChanges {
-//            try context.save()
-//        }
-//    }
     
 }
 
