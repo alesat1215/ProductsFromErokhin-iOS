deb2448ab6c75f9bc1daf39413e1b77a694007d5
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-09-17 21:47:39 +0300

    add AuthComplectionMockTests

---
+++ ProductsFromErokhinTests/Mock/AuthComplectionMockTests.swift
@@ -0,0 +1,23 @@
+//
+//  AuthComplectionMockTests.swift
+//  ProductsFromErokhinTests
+//
+//  Created by Alexander Satunin on 17.09.2020.
+//  Copyright © 2020 Alexander Satunin. All rights reserved.
+//
+
+import XCTest
+@testable import ProductsFromErokhin
+
+class AuthComplectionMockTests: XCTestCase {
+    
+    func testResult() {
+        // Success
+        var complection = AuthComplectionMock()
+        XCTAssertNotNil(try complection.result().toBlocking().first())
+        // Error
+        complection = AuthComplectionMock(error: AppError.unknown)
+        XCTAssertEqual(try complection.result().toBlocking().first()?.error?.localizedDescription, AppError.unknown.localizedDescription)
+    }
+
+}
