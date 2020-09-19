a0d419f634caef56b0173729841c5e0257fdac5c
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-09-19 23:22:19 +0300

    add testProducts2

--- ProductsFromErokhinTests/Start/StartViewModelTests.swift
+++ ProductsFromErokhinTests/Start/StartViewModelTests.swift
@@ -22,15 +22,6 @@ class StartViewModelTests: XCTestCase {
         repository = AppRepositoryMock()
         viewModel = StartViewModel(repository: repository)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
     
     func testTitles() {
         let titles = [Titles(context: context)]
@@ -51,12 +42,17 @@ class StartViewModelTests: XCTestCase {
         XCTAssertNotNil(repository.predicate)
         XCTAssertNotNil(repository.cellId)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testProducts2() {
+        let products = CoreDataSource(observer: AnyObserver(eventHandler: {_ in }), cellId: "", fetchRequest: Product.fetchRequestWithSort(), managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        var result: CoreDataSource<Product>?
+        XCTAssertNil(repository.predicate)
+        XCTAssertNil(repository.cellId)
+        viewModel.products2().subscribe(onNext: { result = $0.element }).disposed(by: disposeBag)
+        repository.productsResult.accept(Event.next(products))
+        XCTAssertEqual(result, products)
+        XCTAssertNotNil(repository.predicate)
+        XCTAssertNotNil(repository.cellId)
     }
 
 }
