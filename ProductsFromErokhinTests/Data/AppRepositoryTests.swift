b6529fc07e537dbb6cd05a6f5bfa729b5d9b836a
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-09-19 22:33:16 +0300

    add testTitles

--- ProductsFromErokhinTests/Data/AppRepositoryTests.swift
+++ ProductsFromErokhinTests/Data/AppRepositoryTests.swift
@@ -11,9 +11,16 @@ import RxSwift
 @testable import ProductsFromErokhin
 
 class AppRepositoryTests: XCTestCase {
+    
+    private var updater: DatabaseUpdaterMock!
+    private var context: ContextMock!
+    private var repository: AppRepository!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        updater = DatabaseUpdaterMock()
+        context = ContextMock()
+        repository = AppRepository(updater: updater, context: context)
     }
 
     override func tearDownWithError() throws {
@@ -26,12 +33,10 @@ class AppRepositoryTests: XCTestCase {
     }
     
     func testProducts() throws {
-        let updater = DatabaseUpdaterMock()
-        let context = ContextMock()
-        let repository = AppRepository(updater: updater, context: context)
+//        let updater = DatabaseUpdaterMock()
+//        let context = ContextMock()
+//        let repository = AppRepository(updater: updater, context: context)
         // Success
-        XCTAssertFalse(updater.isSync)
-        XCTAssertFalse(context.isFetch)
         // Check sequence contains only one element
         XCTAssertThrowsError(try repository.products(cellId: "").take(2).toBlocking(timeout: 1).toArray())
         updater.isSync = false
@@ -46,6 +51,26 @@ class AppRepositoryTests: XCTestCase {
         context.isFetch = false
         updater.error = AppError.unknown
         XCTAssertTrue(try repository.products(cellId: "").take(2).toBlocking().toArray().contains { $0.error?.localizedDescription == AppError.unknown.localizedDescription })
+        XCTAssertTrue(updater.isSync)
+        XCTAssertTrue(context.isFetch)
+    }
+    
+    func testTitles() {
+        // Success
+        // Check sequence contains only one element
+        XCTAssertThrowsError(try repository.titles().take(2).toBlocking(timeout: 1).toArray())
+        updater.isSync = false
+        context.isFetch = false
+        // Check that element
+        XCTAssertNotNil(try repository.titles().toBlocking().first()?.element)
+        XCTAssertTrue(updater.isSync)
+        XCTAssertTrue(context.isFetch)
+        
+        // Sync error
+        updater.isSync = false
+        context.isFetch = false
+        updater.error = AppError.unknown
+        XCTAssertTrue(try repository.titles().take(2).toBlocking().toArray().contains { $0.error?.localizedDescription == AppError.unknown.localizedDescription })
         XCTAssertTrue(updater.isSync)
         XCTAssertTrue(context.isFetch)
     }
