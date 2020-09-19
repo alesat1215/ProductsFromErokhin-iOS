8211b7f3f7cf61463574ec08e94ee7a65fb66e7c
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-09-19 21:02:44 +0300

    CoreDataSourceTests to context mock

--- ProductsFromErokhinTests/Util/CoreDataSourceTests.swift
+++ ProductsFromErokhinTests/Util/CoreDataSourceTests.swift
@@ -12,33 +12,17 @@ import RxCoreData
 @testable import ProductsFromErokhin
 
 class CoreDataSourceTests: XCTestCase {
-    
-    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+    private let context = ContextMock()
     
     private var products = [NSManagedObject]()
     private var dataSource: CoreDataSource<Product>!
     private var collectionView: CollectionViewMock!
     
     override func setUpWithError() throws {
-        // Clear products
-        try Product.clearEntity(context: context)
-        // Add products
-        products = [("product", 0), ("product2", 1), ("product3", 2)].map {
-            let product = NSEntityDescription.insertNewObject(forEntityName: "Product", into: context)
-            product.setValue($0.0, forKey: "name")
-            product.setValue($0.1, forKey: "order")
-            return product
-        }
-        try context.save()
-        
-        dataSource = try context.rx.coreDataSource(cellId: "product", fetchRequest: Product.fetchRequestWithSort()).toBlocking().first()
+        context.fetchResult = [Product](repeating: Product(context: context), count: 3)
+        dataSource = try context.rx.coreDataSource(cellId: "", fetchRequest: Product.fetchRequestWithSort()).toBlocking().first()
         collectionView = CollectionViewMock()
-    }
-
-    override func tearDownWithError() throws {
-        // Clear products
-        try Product.clearEntity(context: context)
-        try context.save()
     }
     
     func testCoreDataSource() throws {
@@ -55,7 +39,7 @@ class CoreDataSourceTests: XCTestCase {
     
     func testUICollectionViewDataSource() {
         // Count of items
-        XCTAssertEqual(dataSource.collectionView(collectionView, numberOfItemsInSection: 0), products.count)
+        XCTAssertEqual(dataSource.collectionView(collectionView, numberOfItemsInSection: 0), context.fetchResult.count)
         // Cell for indexPath
         let cell = dataSource.collectionView(collectionView, cellForItemAt: IndexPath(item: 0, section: 0))
         XCTAssertTrue((cell as! CoreDataCellMock).isBind)
@@ -65,41 +49,41 @@ class CoreDataSourceTests: XCTestCase {
         let fetchRequest = Product.fetchRequestWithSort() as! NSFetchRequest<NSFetchRequestResult>
         let frc: NSFetchedResultsController<NSFetchRequestResult> = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         // Not bind
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .insert, newIndexPath: nil)
+        dataSource.controller(frc, didChange: context.fetchResult.first!, at: nil, for: .insert, newIndexPath: nil)
         XCTAssertFalse(collectionView.isInsert)
         // Bind
         dataSource.bind(collectionView: collectionView)
         // Insert
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .insert, newIndexPath: nil)
+        dataSource.controller(frc, didChange: context.fetchResult.first!, at: nil, for: .insert, newIndexPath: nil)
         XCTAssertFalse(collectionView.isInsert)
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .insert, newIndexPath: .init())
+        dataSource.controller(frc, didChange: context.fetchResult.first!, at: nil, for: .insert, newIndexPath: .init())
         XCTAssertTrue(collectionView.isInsert)
         collectionView.isInsert.toggle()
         // Update
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .update, newIndexPath: nil)
+        dataSource.controller(frc, didChange: context.fetchResult.first!, at: nil, for: .update, newIndexPath: nil)
         XCTAssertFalse(collectionView.cell.isBind)
-        dataSource.controller(frc, didChange: products.first!, at: .init(), for: .update, newIndexPath: nil)
+        dataSource.controller(frc, didChange: context.fetchResult.first!, at: .init(), for: .update, newIndexPath: nil)
         XCTAssertTrue(collectionView.cell.isBind)
         collectionView.cell.isBind.toggle()
         // Move
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .move, newIndexPath: nil)
+        dataSource.controller(frc, didChange: context.fetchResult.first!, at: nil, for: .move, newIndexPath: nil)
         XCTAssertFalse(collectionView.isInsert)
         XCTAssertFalse(collectionView.isDelete)
-        dataSource.controller(frc, didChange: products.first!, at: .init(), for: .move, newIndexPath: .init())
+        dataSource.controller(frc, didChange: context.fetchResult.first!, at: .init(), for: .move, newIndexPath: .init())
         XCTAssertTrue(collectionView.isInsert)
         XCTAssertTrue(collectionView.isDelete)
         collectionView.isInsert.toggle()
         collectionView.isDelete.toggle()
         // Delete
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .delete, newIndexPath: nil)
+        dataSource.controller(frc, didChange: context.fetchResult.first!, at: nil, for: .delete, newIndexPath: nil)
         XCTAssertFalse(collectionView.isDelete)
-        dataSource.controller(frc, didChange: products.first!, at: IndexPath(item: 0, section: 0), for: .delete, newIndexPath: nil)
+        dataSource.controller(frc, didChange: context.fetchResult.first!, at: IndexPath(item: 0, section: 0), for: .delete, newIndexPath: nil)
         XCTAssertTrue(collectionView.isDelete)
         collectionView.isDelete.toggle()
     }
     
     func testObject() {
-        XCTAssertEqual(dataSource.object(at: IndexPath(item: 0, section: 0)), products.first)
+        XCTAssertEqual(dataSource.object(at: IndexPath(item: 0, section: 0)), context.fetchResult.first! as? Product)
         XCTAssertNil(dataSource.object(at: IndexPath(item: 0, section: 7)))
         XCTAssertNil(dataSource.object(at: IndexPath(item: 5, section: 0)))
     }
