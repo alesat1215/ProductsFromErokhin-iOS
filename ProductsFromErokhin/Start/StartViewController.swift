98827151ceee3ad9a1763c984df12ba0e1e071ee
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-09-20 20:26:49 +0300

    ProductCell to file

--- ProductsFromErokhin/Start/StartViewController.swift
+++ ProductsFromErokhin/Start/StartViewController.swift
@@ -93,50 +93,7 @@ class StartViewController: UIViewController {
 }
 
 // MARK: - Cell
-class ProductCell: CoreDataCell<Product> {
-    
-    @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var price: UILabel!
-    @IBOutlet weak var inCart: UILabel!
-    @IBOutlet weak var inCartMarker: UIView!
-    @IBOutlet weak var _del: UIButton!
-    @IBOutlet weak var img: UIImageView!
-    /** Add product to cart */
-    @IBAction func add(_ sender: UIButton) {
-        switch dataSource?.object(at: indexPath)?.addToCart() {
-        case .failure(let error):
-            print(error.localizedDescription)
-        default:
-            print("Product add to cart success")
-        }
-    }
-    /** Del product from cart */
-    @IBAction func del(_ sender: UIButton) {
-        switch dataSource?.object(at: indexPath)?.delFromCart() {
-        case .failure(let error):
-            print(error.localizedDescription)
-        default:
-            print("Product del from cart success")
-        }
-    }
-    
-    override func bind(model: Product, indexPath: IndexPath, dataSource: CoreDataSource<Product>?) {
-        // Model values to views
-        name.text = model.name
-        price.text = "\(model.price) P/Kg"
-        let inCartCount = model.inCart?.count ?? 0
-        inCart.text = "\(inCartCount)"
-        // Set image
-        img.sd_setImage(with: storageReference(path: model.img ?? ""))
-        // Set visible of elements
-        let hidden = inCartCount == 0 ? true : false
-        inCartMarker.isHidden = hidden
-        _del.isHidden = hidden
-        inCart.isHidden = hidden
-        // Set indexPath & dataSource
-        super.bind(model: model, indexPath: indexPath, dataSource: dataSource)
-    }
-}
+
 /** - Returns: Referense for path from Firebase Storage */
 func storageReference(path: String) -> StorageReference {
     SwinjectStoryboard.defaultContainer.resolve(StorageReference.self)!.child(path)
