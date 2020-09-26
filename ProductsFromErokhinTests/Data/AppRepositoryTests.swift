64a5280ddad2d530c775ebe1acd185508a52777a
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-09-26 18:53:15 +0300

    refactor testTitles

--- ProductsFromErokhinTests/Data/AppRepositoryTests.swift
+++ ProductsFromErokhinTests/Data/AppRepositoryTests.swift
@@ -16,6 +16,7 @@ class AppRepositoryTests: XCTestCase {
     private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     private var repository: AppRepository!
     private var products = [Product]()
+    private var titles = [Titles]()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -27,17 +28,43 @@ class AppRepositoryTests: XCTestCase {
             product.name = $0.element
             return product
         }
+        titles = ["title"].map {
+            let titles = Titles(context: context)
+            titles.title = $0
+            return titles
+        }
         repository = AppRepository(updater: updater, context: context)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         try Product.clearEntity(context: context)
+        try Titles.clearEntity(context: context)
     }
 
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testTitles() throws {
+        // Success
+        // Check sequence contains only one element
+        XCTAssertThrowsError(try repository.titles().take(2).toBlocking(timeout: 1).toArray())
+        updater.isSync = false
+        // Check that element
+        var result = try repository.titles().toBlocking().first()?.element
+        XCTAssertTrue(updater.isSync)
+        XCTAssertEqual(result?.count, titles.count)
+    
+        // Sync error
+        updater.isSync = false
+        updater.error = AppError.unknown
+        let resultArray = try repository.titles().take(2).toBlocking().toArray()
+        XCTAssertTrue(resultArray.contains { $0.error?.localizedDescription == AppError.unknown.localizedDescription })
+        XCTAssertTrue(updater.isSync)
+        result = resultArray.first { $0.error == nil }?.element
+        XCTAssertEqual(result?.count, titles.count)
     }
     
     func testProductsCollectionView() throws {
@@ -88,25 +115,7 @@ class AppRepositoryTests: XCTestCase {
         XCTAssertEqual(result?.tableView(UITableView(frame: .init()), numberOfRowsInSection: 0), products.count)
     }
     
-//    func testTitles() {
-//        // Success
-//        // Check sequence contains only one element
-//        XCTAssertThrowsError(try repository.titles().take(2).toBlocking(timeout: 1).toArray())
-//        updater.isSync = false
-//        context.isFetch = false
-//        // Check that element
-//        XCTAssertNotNil(try repository.titles().toBlocking().first()?.element)
-//        XCTAssertTrue(updater.isSync)
-//        XCTAssertTrue(context.isFetch)
-//
-//        // Sync error
-//        updater.isSync = false
-//        context.isFetch = false
-//        updater.error = AppError.unknown
-//        XCTAssertTrue(try repository.titles().take(2).toBlocking().toArray().contains { $0.error?.localizedDescription == AppError.unknown.localizedDescription })
-//        XCTAssertTrue(updater.isSync)
-//        XCTAssertTrue(context.isFetch)
-//    }
+
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
