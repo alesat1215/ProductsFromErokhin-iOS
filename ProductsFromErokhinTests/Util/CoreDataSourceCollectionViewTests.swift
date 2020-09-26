ffc30d43b6130f15650e3656ab7ee9a0473c6c1c
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-09-26 19:23:06 +0300

    refactor CoreDataSourceCollectionViewTests

--- ProductsFromErokhinTests/Util/CoreDataSourceCollectionViewTests.swift
+++ ProductsFromErokhinTests/Util/CoreDataSourceCollectionViewTests.swift
@@ -16,20 +16,29 @@ class CoreDataSourceCollectionViewTests: XCTestCase {
     private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     private var products = [Product]()
-    private var dataSource: CoreDataSourceCollectionView<Product>!
+    private var groups = [Group]()
+    private var dataSourceProducts: CoreDataSourceCollectionView<Product>!
+    private var dataSourceGroups: CoreDataSourceCollectionView<Group>!
     private var collectionView: CollectionViewMock!
     
     override func setUpWithError() throws {
         // Add products to context
-        let product = Product(context: context)
-        product.order = 0
-        let product1 = Product(context: context)
-        product1.order = 1
-        let product2 = Product(context: context)
-        product2.order = 2
-        products = [product, product1, product2]
+        products = ["product", "product1", "product2"].enumerated().map {
+            let product = Product(context: context)
+            product.order = Int16($0.offset)
+            product.name = $0.element
+            return product
+        }
         
-        dataSource = try context.rx.coreDataSource(cellId: "", fetchRequest: Product.fetchRequestWithSort()).toBlocking().first()
+        groups = ["group", "group1", "group2"].enumerated().map {
+            let group = Group(context: context)
+            group.order = Int16($0.offset)
+            group.name = $0.element
+            return group
+        }
+        
+        dataSourceProducts = try context.rx.coreDataSource(cellId: "", fetchRequest: Product.fetchRequestWithSort()).toBlocking().first()
+        dataSourceGroups = try context.rx.coreDataSource(cellId: "", fetchRequest: Group.fetchRequestWithSort()).toBlocking().first()
         collectionView = CollectionViewMock()
     }
     
@@ -39,22 +48,22 @@ class CoreDataSourceCollectionViewTests: XCTestCase {
     }
     
     func testCoreDataSource() throws {
-        XCTAssertNotNil(dataSource)
+        XCTAssertNotNil(dataSourceProducts)
     }
     
     func testBind() {
         XCTAssertNil(collectionView.dataSource)
         XCTAssertFalse(collectionView.isReload)
-        dataSource.bind(collectionView: collectionView)
+        dataSourceProducts.bind(collectionView: collectionView)
         XCTAssertNotNil(collectionView.dataSource)
         XCTAssertTrue(collectionView.isReload)
     }
     
     func testUICollectionViewDataSource() {
         // Count of items
-        XCTAssertEqual(dataSource.collectionView(collectionView, numberOfItemsInSection: 0), products.count)
+        XCTAssertEqual(dataSourceProducts.collectionView(collectionView, numberOfItemsInSection: 0), products.count)
         // Cell for indexPath
-        let cell = dataSource.collectionView(collectionView, cellForItemAt: IndexPath(item: 0, section: 0))
+        let cell = dataSourceProducts.collectionView(collectionView, cellForItemAt: IndexPath(item: 0, section: 0))
         XCTAssertTrue((cell as! CollectionViewCellMock).isBind)
     }
     
@@ -62,77 +71,67 @@ class CoreDataSourceCollectionViewTests: XCTestCase {
         let fetchRequest = Product.fetchRequestWithSort() as! NSFetchRequest<NSFetchRequestResult>
         let frc: NSFetchedResultsController<NSFetchRequestResult> = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         // Not bind
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .insert, newIndexPath: nil)
+        dataSourceProducts.controller(frc, didChange: products.first!, at: nil, for: .insert, newIndexPath: nil)
         XCTAssertFalse(collectionView.isInsert)
         // Bind
-        dataSource.bind(collectionView: collectionView)
+        dataSourceProducts.bind(collectionView: collectionView)
         // Insert
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .insert, newIndexPath: nil)
+        dataSourceProducts.controller(frc, didChange: products.first!, at: nil, for: .insert, newIndexPath: nil)
         XCTAssertFalse(collectionView.isInsert)
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .insert, newIndexPath: .init())
+        dataSourceProducts.controller(frc, didChange: products.first!, at: nil, for: .insert, newIndexPath: .init())
         XCTAssertTrue(collectionView.isInsert)
         collectionView.isInsert.toggle()
         // Update
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .update, newIndexPath: nil)
+        dataSourceProducts.controller(frc, didChange: products.first!, at: nil, for: .update, newIndexPath: nil)
         XCTAssertFalse(collectionView.cell.isBind)
-        dataSource.controller(frc, didChange: products.first!, at: .init(), for: .update, newIndexPath: nil)
+        dataSourceProducts.controller(frc, didChange: products.first!, at: .init(), for: .update, newIndexPath: nil)
         XCTAssertTrue(collectionView.cell.isBind)
         collectionView.cell.isBind.toggle()
         // Move
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .move, newIndexPath: nil)
+        dataSourceProducts.controller(frc, didChange: products.first!, at: nil, for: .move, newIndexPath: nil)
         XCTAssertFalse(collectionView.isInsert)
         XCTAssertFalse(collectionView.isDelete)
-        dataSource.controller(frc, didChange: products.first!, at: .init(), for: .move, newIndexPath: .init())
+        dataSourceProducts.controller(frc, didChange: products.first!, at: .init(), for: .move, newIndexPath: .init())
         XCTAssertTrue(collectionView.isInsert)
         XCTAssertTrue(collectionView.isDelete)
         collectionView.isInsert.toggle()
         collectionView.isDelete.toggle()
         // Delete
-        dataSource.controller(frc, didChange: products.first!, at: nil, for: .delete, newIndexPath: nil)
+        dataSourceProducts.controller(frc, didChange: products.first!, at: nil, for: .delete, newIndexPath: nil)
         XCTAssertFalse(collectionView.isDelete)
-        dataSource.controller(frc, didChange: products.first!, at: IndexPath(item: 0, section: 0), for: .delete, newIndexPath: nil)
+        dataSourceProducts.controller(frc, didChange: products.first!, at: IndexPath(item: 0, section: 0), for: .delete, newIndexPath: nil)
         XCTAssertTrue(collectionView.isDelete)
         collectionView.isDelete.toggle()
     }
     
     func testObject() {
-        XCTAssertEqual(dataSource.object(at: IndexPath(item: 0, section: 0)), products.first!)
-        XCTAssertNil(dataSource.object(at: IndexPath(item: 0, section: 7)))
-        XCTAssertNil(dataSource.object(at: IndexPath(item: 5, section: 0)))
+        XCTAssertEqual(dataSourceProducts.object(at: IndexPath(item: 0, section: 0)), products.first!)
+        XCTAssertNil(dataSourceProducts.object(at: IndexPath(item: 0, section: 7)))
+        XCTAssertNil(dataSourceProducts.object(at: IndexPath(item: 5, section: 0)))
     }
     
     func testIndexPath() {
-        XCTAssertNotNil(dataSource.indexPath(for: products.first!))
-        XCTAssertNil(dataSource.indexPath(for: Product(context: context)))
+        XCTAssertNotNil(dataSourceProducts.indexPath(for: products.first!))
+        XCTAssertNil(dataSourceProducts.indexPath(for: Product(context: context)))
     }
     
     func testSelect() throws {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
-        let group = Group(context: context)
-        group.order = 0
-        group.isSelected = true
-        let group1 = Group(context: context)
-        group1.order = 1
-        let group2 = Group(context: context)
-        group2.order = 2
-        
-        let dataSource: CoreDataSourceCollectionView<Group> = try context.rx.coreDataSource(cellId: "group", fetchRequest: Group.fetchRequestWithSort()).toBlocking().first()!
+        groups.first?.isSelected = true
         
         expectation(forNotification: .NSManagedObjectContextDidSave, object: context)
         
-        XCTAssertNoThrow(try dataSource.select(indexPath: IndexPath(item: 1, section: 0)).get())
+        XCTAssertNoThrow(try dataSourceGroups.select(indexPath: IndexPath(item: 1, section: 0)).get())
         
-        XCTAssertFalse(group.isSelected)
-        XCTAssertTrue(group1.isSelected)
+        XCTAssertFalse(groups.first!.isSelected)
+        XCTAssertTrue(groups[1].isSelected)
         
         waitForExpectations(timeout: 1)
     }
     
     func testDispose() {
-        dataSource.bind(collectionView: collectionView)
+        dataSourceProducts.bind(collectionView: collectionView)
         XCTAssertNotNil(collectionView.dataSource)
-        dataSource.dispose()
+        dataSourceProducts.dispose()
         XCTAssertNil(collectionView.dataSource)
     }
 
