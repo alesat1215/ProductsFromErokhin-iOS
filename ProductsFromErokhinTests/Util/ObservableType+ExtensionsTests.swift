be9b0a1f17ef4d4ee24454ea258b5291eaa65667
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-03 19:27:36 +0300

    add testFlatMapErrorObservableHandler

--- ProductsFromErokhinTests/Util/ObservableType+ExtensionsTests.swift
+++ ProductsFromErokhinTests/Util/ObservableType+ExtensionsTests.swift
@@ -13,7 +13,7 @@ import RxBlocking
 @testable import ProductsFromErokhin
 
 class ObservableType_ExtensionsTests: XCTestCase {
-
+    // func flatMapError(_ handler: ((_ error: Error) -> ())? = nil) -> Observable<Element.Element>
     func testFlatMapError() {
         // Success
         let test = "test"
@@ -23,6 +23,44 @@ class ObservableType_ExtensionsTests: XCTestCase {
         testObservable = Observable.just(Event.error(AppError.unknown))
         var error: Error?
         var result = testObservable.flatMapError { error = $0 }.toBlocking().materialize()
+        switch result {
+        case .completed(elements: let elements):
+            XCTAssertTrue(elements.isEmpty)
+        default:
+            XCTFail("Must be completed")
+        }
+        XCTAssertEqual(error?.localizedDescription, AppError.unknown.localizedDescription)
+        // Empty
+        testObservable = Observable.empty()
+        result = testObservable.flatMapError().toBlocking().materialize()
+        switch result {
+        case .completed(elements: let elements):
+            XCTAssertTrue(elements.isEmpty)
+        default:
+            XCTFail("Must be completed")
+        }
+    }
+    
+    // func flatMapError<T>(_ handler: ((_ error: Error) -> Observable<T>)? = nil) -> Observable<Element.Element>
+    func testFlatMapErrorObservableHandler() {
+        // Success
+        let test = "test"
+        var testObservable = Observable.just(Event.next(test))
+        XCTAssertEqual(test, try testObservable.flatMapError().toBlocking().first())
+        // Error
+        let disposeBag = DisposeBag()
+        testObservable = Observable.just(Event.error(AppError.unknown))
+        var error: Error?
+        let handlerObservable = PublishSubject<Error>()
+        func testHandlerObservable(error: Error) -> Observable<Error> {
+            handlerObservable.onNext(error)
+            handlerObservable.onCompleted()
+            return handlerObservable.asObservable()
+        }
+        handlerObservable.subscribe(onNext: {
+            error = $0
+        }).disposed(by: disposeBag)
+        var result = testObservable.flatMapError { testHandlerObservable(error: $0) }.toBlocking().materialize()
         switch result {
         case .completed(elements: let elements):
             XCTAssertTrue(elements.isEmpty)
