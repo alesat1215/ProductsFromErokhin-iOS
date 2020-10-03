149498bc15375da22d6aa0f420bb4caf32d1557b
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-03 19:59:02 +0300

    add testActivity

---
+++ ProductsFromErokhinTests/Util/UIViewController+RxTests.swift
@@ -0,0 +1,61 @@
+//
+//  UIViewController+RxTests.swift
+//  ProductsFromErokhinTests
+//
+//  Created by Alexander Satunin on 03.10.2020.
+//  Copyright © 2020 Alexander Satunin. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+@testable import ProductsFromErokhin
+
+class UIViewController_RxTests: XCTestCase {
+    
+    private var controller: UIViewController!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        controller = UIViewController()
+        let window = UIWindow()
+        window.rootViewController = controller
+        window.makeKeyAndVisible()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testActivity() {
+        XCTAssertNil(controller.presentedViewController)
+        let disposeBag = DisposeBag()
+        var result: (Bool, Error?)?
+        controller.rx.activity(activityItems: []).subscribe(onNext: {
+            result = $0
+        }).disposed(by: disposeBag)
+        XCTAssertNotNil(controller.presentedViewController)
+        let activityController = controller.presentedViewController as! UIActivityViewController
+        XCTAssertNil(result)
+        // First call
+        activityController.completionWithItemsHandler?(nil, true, nil, AppError.unknown)
+        XCTAssertTrue(result!.0)
+        XCTAssertEqual(result?.1?.localizedDescription, AppError.unknown.localizedDescription)
+        // Second call. Result must not be changed, because sequence is completed
+        activityController.completionWithItemsHandler?(nil, false, nil, AppError.group)
+        XCTAssertTrue(result!.0)
+        XCTAssertEqual(result?.1?.localizedDescription, AppError.unknown.localizedDescription)
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
