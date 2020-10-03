f1194b68d1d700f51c6fdfd46df9a1ac3682dab0
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-03 21:10:19 +0300

    del comments

--- ProductsFromErokhinTests/Util/UIViewController+RxTests.swift
+++ ProductsFromErokhinTests/Util/UIViewController+RxTests.swift
@@ -15,15 +15,10 @@ class UIViewController_RxTests: XCTestCase {
     private var controller: UIViewController!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
         controller = UIViewController()
         let window = UIWindow()
         window.rootViewController = controller
         window.makeKeyAndVisible()
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
     func testActivity() {
@@ -92,18 +87,6 @@ class UIViewController_RxTests: XCTestCase {
         handler(action)
         // Check result
         XCTAssertNil(result)
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
