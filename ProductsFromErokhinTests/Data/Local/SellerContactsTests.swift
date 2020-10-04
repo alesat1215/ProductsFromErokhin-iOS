daa6f1b22260faf34c0989fc2cfb8b32007e6050
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-04 16:47:25 +0300

    add SellerContactsTests

---
+++ ProductsFromErokhinTests/Data/Local/SellerContactsTests.swift
@@ -0,0 +1,32 @@
+//
+//  SellerContactsTests.swift
+//  ProductsFromErokhinTests
+//
+//  Created by Alexander Satunin on 04.10.2020.
+//  Copyright © 2020 Alexander Satunin. All rights reserved.
+//
+
+import XCTest
+import CoreData
+@testable import ProductsFromErokhin
+
+class SellerContactsTests: XCTestCase {
+
+    func testUpdate() {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let sellerContactsRemote = SellerContactsRemote(phone: "phone", address: "address")
+        let order = 1
+        
+        let sellerContacts = SellerContacts(context: context)
+        
+        expectation(forNotification: .NSManagedObjectContextDidSave, object: context).isInverted = true
+        
+        sellerContacts.update(from: sellerContactsRemote, order: order)
+        XCTAssertEqual(sellerContacts.order, Int16(order))
+        XCTAssertEqual(sellerContacts.phone, sellerContactsRemote.phone)
+        XCTAssertEqual(sellerContacts.address, sellerContactsRemote.address)
+        
+        waitForExpectations(timeout: 1)
+    }
+
+}
