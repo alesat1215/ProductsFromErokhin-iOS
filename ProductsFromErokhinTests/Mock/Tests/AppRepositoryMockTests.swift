2ee8f3a623b5f8fe27dffe52263ac0498687c0e5
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-04 19:44:05 +0300

    add testSellerContacts

--- ProductsFromErokhinTests/Mock/Tests/AppRepositoryMockTests.swift
+++ ProductsFromErokhinTests/Mock/Tests/AppRepositoryMockTests.swift
@@ -47,17 +47,9 @@ class AppRepositoryMockTests: XCTestCase {
     func testOrderWarning() {
         XCTAssertEqual(try repository.orderWarning().toBlocking().first()?.element, repository.orderWarningResult)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSellerContacts() {
+        XCTAssertEqual(try repository.sellerContacts().toBlocking().first()?.element, repository.sellerContactsResult)
     }
 
 }
