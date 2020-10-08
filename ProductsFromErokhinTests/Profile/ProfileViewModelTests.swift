38b2a5077fef13e1680cfd4d6cca3315c1ccafd1
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-08 22:13:41 +0300

    add ProfileViewModelTests

--- ProductsFromErokhinTests/Profile/ProfileViewModelTests.swift
+++ ProductsFromErokhinTests/Profile/ProfileViewModelTests.swift
@@ -19,27 +19,15 @@ class ProfileViewModelTests: XCTestCase {
         repository = AppRepositoryMock()
         viewModel = ProfileViewModelImpl(repository: repository)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
     
     func testProfile() {
         XCTAssertNil(try viewModel.profile().toBlocking().first())
         repository.profileResult.append(Profile(context: context))
         XCTAssertNotNil(try viewModel.profile().toBlocking().first())
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testUpdateProfile() {
+        XCTAssertNoThrow(try viewModel.updateProfile(name: "", phone: "", address: "").get())
     }
 
 }
