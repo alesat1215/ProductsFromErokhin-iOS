d6b03aa6ba79c43c64ea6da0e211ff8f13a70dc0
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-08 21:40:57 +0300

    del comments

--- ProductsFromErokhinTests/Data/Local/ProfileTests.swift
+++ ProductsFromErokhinTests/Data/Local/ProfileTests.swift
@@ -11,14 +11,6 @@ import CoreData
 @testable import ProductsFromErokhin
 
 class ProfileTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
     
     func testDelivery() {
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -54,18 +46,30 @@ class ProfileTests: XCTestCase {
         profile.phone = nil
         profile.address = "address"
         XCTAssertEqual(profile.delivery(), "\r\n\r\naddress")
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        
+        // Name, phone
+        profile.name = "name"
+        profile.phone = "phone"
+        profile.address = nil
+        XCTAssertEqual(profile.delivery(), "\r\n\r\nname\r\nphone")
+        
+        // Name, address
+        profile.name = "name"
+        profile.phone = nil
+        profile.address = "address"
+        XCTAssertEqual(profile.delivery(), "\r\n\r\nname\r\naddress")
+        
+        // Phone, address
+        profile.name = nil
+        profile.phone = "phone"
+        profile.address = "address"
+        XCTAssertEqual(profile.delivery(), "\r\n\r\nphone\r\naddress")
+        
+        // Name, phone, address
+        profile.name = "name"
+        profile.phone = "phone"
+        profile.address = "address"
+        XCTAssertEqual(profile.delivery(), "\r\n\r\nname\r\nphone\r\naddress")
     }
 
 }
