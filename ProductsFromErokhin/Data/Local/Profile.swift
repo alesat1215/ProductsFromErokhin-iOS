bd54ff3a72233a2130783689d7d924bbb446e9fe
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-08 19:52:48 +0300

    add delivery()

--- ProductsFromErokhin/Data/Local/Profile.swift
+++ ProductsFromErokhin/Data/Local/Profile.swift
@@ -9,5 +9,18 @@
 import Foundation
 
 extension Profile: Ordered {
-    
+    func delivery() -> String {
+        guard let name = name, name.isEmpty,
+              let phone = phone, phone.isEmpty,
+              let address = address, address.isEmpty
+        else {
+            return ""
+        }
+        let separator = "\r\n"
+        var result = ""
+        result += separator + separator + name
+        result += { if result.last != separator.last && !phone.isEmpty { return separator } else { return "" } }() + phone
+        result += { if result.last != separator.last && !address.isEmpty { return separator } else { return "" } }() + address
+        return result
+    }
 }
