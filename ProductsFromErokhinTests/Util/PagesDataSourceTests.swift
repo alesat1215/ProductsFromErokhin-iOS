eb52816c6281c580b3892d8748afa192b6ccb0eb
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-11 15:44:52 +0300

    del comments

--- ProductsFromErokhinTests/Util/PagesDataSourceTests.swift
+++ ProductsFromErokhinTests/Util/PagesDataSourceTests.swift
@@ -113,17 +113,5 @@ class PagesDataSourceTests: XCTestCase {
         controller.setViewControllers([page1], direction: .forward, animated: true)
         XCTAssertEqual(dataSource.presentationIndex(for: controller), 0)
     }
-    
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
