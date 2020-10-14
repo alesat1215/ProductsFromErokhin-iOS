125bda3abeb967738aecb1bd82809237de0e50fc
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-14 15:03:16 +0300

    add testSetupCallAction

--- ProductsFromErokhinTests/Contacts/ContactsViewControllerTests.swift
+++ ProductsFromErokhinTests/Contacts/ContactsViewControllerTests.swift
@@ -93,17 +93,13 @@ class ContactsViewControllerTests: XCTestCase {
         XCTAssertEqual(controller.phone.text, contacts.phone)
         XCTAssertEqual(controller.address.text, contacts.address)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSetupCallAction() {
+        viewModel.contactsResult.accept(Event.next([contacts]))
+        
+        controller.call.sendActions(for: .touchUpInside)
+        XCTAssertTrue(viewModel.isCall)
+        XCTAssertEqual(viewModel.phoneResult, contacts.phone)
     }
 
 }
