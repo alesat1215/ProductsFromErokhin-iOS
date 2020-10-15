89457f368dd4974b4228425edad7bb874c822a8d
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-15 21:57:04 +0300

    refactor MoreTableViewControllerTests

--- ProductsFromErokhinTests/More/MoreTableViewControllerTests.swift
+++ ProductsFromErokhinTests/More/MoreTableViewControllerTests.swift
@@ -40,7 +40,7 @@ class MoreTableViewControllerTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try AboutApp.clearEntity(context: context)
     }
     
     func testSetupShareAction() {
@@ -103,18 +103,6 @@ class MoreTableViewControllerTests: XCTestCase {
         XCTAssertNotNil(controller.presentedViewController)
         XCTAssertTrue(controller.presentedViewController is UIActivityViewController)
         XCTAssertTrue(viewModel.isAboutApp)
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
