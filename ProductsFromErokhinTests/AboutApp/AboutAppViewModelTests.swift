6793dfc85b749ea591fbd1ac5638c5cf68ae743f
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-15 10:37:04 +0300

    del comments

--- ProductsFromErokhinTests/AboutApp/AboutAppViewModelTests.swift
+++ ProductsFromErokhinTests/AboutApp/AboutAppViewModelTests.swift
@@ -17,10 +17,6 @@ class AboutAppViewModelTests: XCTestCase {
         repository = AppRepositoryMock()
         viewModel = AboutAppViewModelImpl(repository: repository)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
     
     func testAboutApp() {
         XCTAssertEqual(try viewModel.aboutApp().dematerialize().toBlocking().first(), repository.aboutAppResult)
@@ -33,18 +29,6 @@ class AboutAppViewModelTests: XCTestCase {
     func testVersion() {
         let version = "\(Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String) (\(Bundle.main.infoDictionary!["CFBundleVersion"] as! String))"
         XCTAssertEqual(viewModel.version(), version)
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
