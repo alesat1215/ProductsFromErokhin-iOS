d64933fc8ad558277f70abe35bc01807d2ba95b8
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-10-22 20:32:22 +0300

    add comments

--- ProductsFromErokhin/Util/NWPathMonitor+Rx.swift
+++ ProductsFromErokhin/Util/NWPathMonitor+Rx.swift
@@ -12,19 +12,20 @@ import RxSwift
 
 extension NWPathMonitor: NWPathMonitorMethods { }
 extension NWPath: NWPathMethods { }
-
+/** NWPathMonitor methods */
 protocol NWPathMonitorMethods: AnyObject, ReactiveCompatible {
     associatedtype P: NWPathMethods
     var pathUpdateHandler: ((P) -> Void)? { get set }
     func start(queue: DispatchQueue)
     func cancel()
 }
-
+/** NWPath methods */
 protocol NWPathMethods {
     var status: NWPath.Status { get }
 }
-
+/** Reactive wrapper for NWPathMonitor methods */
 extension Reactive where Base: NWPathMonitorMethods {
+    /** Generate true  */
     internal func status() -> Observable<Bool> {
         Observable.create { observer in
             base.pathUpdateHandler = { path in
